0a31b4444c9ec54dbb8b74fc1dc01c4b124d5f97
surpass1116 <greenrikun16@gmail.com>
2025-01-06 01:26:02 +0900

    書き換えました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -3,10 +3,115 @@
 
 #include "../include/lib_func.h"
 
+void qsort_parts(item* begin, item* end)
+{
+	if (end - begin < 1) return;
+
+	bool beginflag = 0, endflag = 0;
+	item  *keepend = (end-1);
+	item* keepbegin = begin;
+	item  *pivot = begin;
+
+
+	while (1) {
+		if (!beginflag) {
+			begin++;
+			if (begin->key > pivot->key) {
+				beginflag = 1;
+			}
+		}
+		if (!endflag) {
+			end--;
+			if (end->key < pivot->key) {
+				endflag = 1;
+			}
+		}
+		if (begin > end) {
+			item a = *pivot;
+			*pivot = *end;
+			*end = a;
+
+			if (end - keepbegin > 1)
+				qsort_parts(keepbegin, end);
+
+			if ((end+1) - (keepbegin+1) > 1)
+				qsort_parts(end + 1, keepend+1);
+
+			break;
+		}
+		else {
+			if (beginflag && endflag) {
+				item a = *begin;
+				*begin = *end;
+				*end = a;
+				beginflag = 0;
+				endflag = 0;
+			}
+
+		}
+	}
+}
+
 // クイックソート(引数が不適切であればfalseを返す)
 bool quick_sort(item* begin, const item* end)
 {
 	// ToDo: クイックソートで並び替えてください
+	if (begin == NULL)return false;
+	if (end == NULL)return false;
+	if (end < begin)return false;
 
-	return false;
+	/*item* n, * m;
+
+
+	n = begin;
+	m = begin + (end - begin);*/
+
+
+
+	//qsort_parts(n, m);
+	bool beginflag = 0, endflag = 0;
+	item* keepend = (begin + (end - begin) - 1);
+	item* keepbegin = begin;
+	item* pivot = begin;
+
+
+	while (1) {
+		if (!beginflag) {
+			begin++;
+			if (begin->key > pivot->key) {
+				beginflag = 1;
+			}
+		}
+		if (!endflag) {
+			end--;
+			if (end->key < pivot->key) {
+				endflag = 1;
+			}
+		}
+		if (begin > end) {
+			item a = *pivot;
+			*pivot = *end;
+			*(begin + (end - begin)) = a;
+
+			if (end - keepbegin > 1)
+				quick_sort(keepbegin, begin + (end - begin));
+
+			if ((end + 1) - (keepbegin + 1) > 1)
+				quick_sort((begin + (end - begin)) + 1, keepend + 1);
+
+			break;
+		}
+		else {
+			if (beginflag && endflag) {
+				item a = *begin;
+				*begin = *end;
+				*(begin + (end - begin)) = a;
+				beginflag = 0;
+				endflag = 0;
+			}
+
+		}
+	}
+
+	return true;
 }
